a63725c49b69a0e888564745c98a6f5af9c9a38f
Shota Shimazu <hornet.live.mf@gmail.com>
2021-10-14 22:10:55 +0900

    add content view

---
+++ SwiftUI+SceneDelegate/iOSTemplate/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iOSTemplate
+//
+//  Created by Shota Shimazu on 2021/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
