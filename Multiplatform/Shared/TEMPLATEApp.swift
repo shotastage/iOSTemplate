5ea87e8314323a98f4570ab0a773688efc482401
Shota Shimazu <hornet.live.mf@gmail.com>
2022-01-24 14:02:58 +0900

    add new tempplate

---
+++ Multiplatform/Shared/TEMPLATEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEMPLATEApp.swift
+//  Shared
+//
+//  Created by Shota Shimazu on 2022/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TEMPLATEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
